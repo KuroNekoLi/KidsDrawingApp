e7fafc32141f06b05cf91c425046adf8fc42a731
KuroNekoLi <git@github.com:KuroNekoLi/room.git>
2023-05-22 13:47:38 +0800

    增加權限請求與從相簿選取

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.kidsdrawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
@@ -12,7 +20,35 @@ import com.example.kidsdrawingapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var mImageButtonCurrentPaint : ImageButton? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+    //compileSdkVersion 必須31以上
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    Toast.makeText(this, "權限已許可", Toast.LENGTH_LONG).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "你拒絕了此權限", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+    private val openGalleryLauncher : ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data!=null){
+                binding.ivBackground.setImageURI(result.data?.data)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,20 +58,22 @@ class MainActivity : AppCompatActivity() {
 
         mImageButtonCurrentPaint = binding.llPaintColors[2] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
         binding.drawingView.setSizeForBrush(20.toFloat())
         binding.ibBrush.setOnClickListener { showBrushSizeChooserDialog() }
+
+        binding.ibGallery.setOnClickListener {requestStoragePermission() }
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size:")
-        val smallBtn : ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
-        val mediumBtn : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
-        val largeBtn : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
         smallBtn.setOnClickListener {
             binding.drawingView.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
@@ -51,21 +89,47 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    fun paintClicked(view: View){
-        if(view != mImageButtonCurrentPaint){
+    fun paintClicked(view: View) {
+        if (view != mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             binding.drawingView.setColor(colorTag)
 
             imageButton.setImageDrawable(
-                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
             )
 
             mImageButtonCurrentPaint?.setImageDrawable(
-                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
             )
 
             mImageButtonCurrentPaint = view
+        }
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            showRationaleDialog("Kids Drawing App","該App需要您的外部存取權限")
+        }else{
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+                //TODO - Add writing external storage permission
+            ))
         }
     }
 }
